e831ef2147c459bd96eb67365cead0f44a4a547c
Rodrigo <rdg.hora1@gmail.com>
2023-12-07 09:48:36 -0300

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export default {
   url: 'https://strapi-v4-test.herokuapp.com/api/pages/?populate=deep&',
-  siteName: 'Otávio Miranda',
+  siteName: 'Rodrigo Silva',
   defaultSlug: 'landing-page',
 };
